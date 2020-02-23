1f74882e3e9eded0ee2823ffdf0b32dada0cd5e9
SwornimBajracharya <so.0nim04@gmail.com>
2020-02-22 16:42:25 -0800

    Added buttons and Activity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,13 +3,30 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private int count=0;
+    private TextView mShowCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.TextBox);
         //This is a test text comment
+    }
+
+    public void showToast(View view){
+        Toast toast = Toast.makeText(this, R.string.ToastMessage, Toast.LENGTH_SHORT);
+        mShowCount.setText("Hello World!");
+        toast.show();
+    }
+
+    public void countUp(View view){
+        count++;
+        mShowCount.setText(Integer.toString(count));
     }
 }
